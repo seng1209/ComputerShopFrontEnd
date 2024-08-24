917622926baf6ee43ef94948158fc77aca9b9b6e
Group1 <hongpink473@gmail.com>
2024-08-24 14:25:00 +0700

    Computer Shop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Link, BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import HomePage from "./component/layout/Layout";
+import MenuPage from "./component/menu/MenuPage";
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+const { Header, Content, Footer, Sider } = Layout;
+const siderStyle = {
+  overflow: "auto",
+  height: "100vh",
+  position: "fixed",
+  insetInlineStart: 0,
+  top: 0,
+  bottom: 0,
+  scrollbarWidth: "thin",
+  scrollbarColor: "unset",
+};
+const App = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <Layout hasSider>
+        <Sider style={siderStyle}>
+          <div className="demo-logo-vertical" />
+          <HomePage />
+          {/* <Outlet /> */}
+        </Sider>
+        <Layout
+          style={{
+            marginInlineStart: 200,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Header
+            style={{
+              padding: 0,
+              background: colorBgContainer,
+            }}
+          />
+          <Content
+            style={{
+              margin: "24px 16px 0",
+              overflow: "initial",
+            }}
+          >
+            <div
+              style={{
+                padding: 24,
+                textAlign: "center",
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              <MenuPage />
+              {/* <Outlet /> */}
+            </div>
+          </Content>
+          <Footer
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
